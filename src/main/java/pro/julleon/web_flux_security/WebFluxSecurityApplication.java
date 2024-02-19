74d776099be694d0f1bfc117a0b8ba5e2e23c62c
Julleon <julleonchik@gmail.com>
2024-02-19 20:04:15 +0900

    initial initialization of the spring_web_flux_security project

---
+++ src/main/java/pro/julleon/web_flux_security/WebFluxSecurityApplication.java
@@ -0,0 +1,13 @@
+package pro.julleon.web_flux_security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebFluxSecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebFluxSecurityApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pro/julleon/web_flux_security/WebFluxSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.julleon.web_flux_security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebFluxSecurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
